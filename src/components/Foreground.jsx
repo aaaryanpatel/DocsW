5c1b4f03e4453e2953dfe5744a734f48600245db
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-18 01:24:37 -0600

    Completed thing propor to framer motion

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -10,12 +10,25 @@ const data = [
         tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green"},
     },
 
+    {
+        desc: "This is the background color of the card that will be deployed.",
+        filesize: ".9mb",
+        close: true,
+        tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green"},
+    },
+    
+    {
+        desc: "This is the background color of the card that will be deployed.",
+        filesize: ".9mb",
+        close: true,
+        tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue"},
+    },
 ]
 
     
   return (
     
-      <div className='fixed z-[3] top-0 left-0  w-full h-full flex '>
+      <div className='fixed z-[3] top-0 left-0  w-full h-full flex gap-5 flex-wrap p-5'>
         {data.map((item, index)=>( 
             <Card data={item} />
         ))}
