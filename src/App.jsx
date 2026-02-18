80ce7978544ccbbd9b431945593e29f839f3d1b9
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-18 00:16:55 -0600

    Added a foreground

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Background from './components/Background'
+import Foreground from './components/Foreground'
 
 export default function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800 '>
       <Background />
-      <div className='fixed z-[3] top-0 left-0  w-full h-full bg-sky-800/50'></div>
+      <Foreground />
     </div>
   )
 }
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Foreground() {
+  return (
+    <div>
+      <div className='fixed z-[3] top-0 left-0  w-full h-full bg-sky-800/50'>
+      </div>
+    </div>
+  )
+}
+
+export default Foreground
